e34b3de2acd182d1aba45f38707d240223b1a4aa
Jalil David Salamé Messina <jalil.salame@tum.de>
2023-03-23 17:51:45 -0300

    feat: Add criterion benchmarks to compare both DFS solvers

---
+++ libnsudoku-solver/benches/sudoku_solver.rs
@@ -0,0 +1,31 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use libnsudoku_solver::{
+    solver::{self, Solver},
+    Sudoku,
+};
+
+pub fn dfs(c: &mut Criterion) {
+    let mut iterative = solver::IterativeDfs::default();
+    let mut recursive = solver::RecursiveDfs::default();
+    let mut group = c.benchmark_group("DFS: Empty Sudoku");
+    
+    let grid_w = 2;
+    let sq_gw = grid_w * grid_w;
+    let empty = Sudoku::empty(grid_w);
+    
+    group.bench_with_input(
+        BenchmarkId::new(format!("Iterative {sq_gw}×{sq_gw} Sudoku"), sq_gw * sq_gw),
+        &empty,
+        |b, empty| b.iter(|| iterative.solve(empty.clone())),
+    );
+    group.bench_with_input(
+        BenchmarkId::new(format!("Recursive {sq_gw}×{sq_gw} Sudoku"), sq_gw * sq_gw),
+        &empty,
+        |b, empty| b.iter(|| recursive.solve(empty.clone())),
+    );
+
+    group.finish();
+}
+
+criterion_group!(benches, dfs);
+criterion_main!(benches);
